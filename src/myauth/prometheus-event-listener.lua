e95ef92da45eae474bdbf9d6462d52a49eb0c811
Andrey O <ozzy.ext@gmail.com>
2021-01-22 12:51:47 +0300

    Add 'server' label into auth metrics

--- src/myauth/prometheus-event-listener.lua
+++ src/myauth/prometheus-event-listener.lua
@@ -6,71 +6,71 @@ function PriometheusEventListener.new(prometheus)
 
 	local new_obj = setmetatable({}, mt)
 
-  	new_obj.metric_allowed = prometheus:counter("myauth_allow_total", "Number of allowed requests", {"url", "reason"})
-	new_obj.metric_denied = prometheus:counter("myauth_deny_total", "Number of denied requests", {"url", "reason"})
+  	new_obj.metric_allowed = prometheus:counter("myauth_allow_total", "Number of allowed requests", {"server", "url", "reason"})
+	new_obj.metric_denied = prometheus:counter("myauth_deny_total", "Number of denied requests", {"server", "url", "reason"})
 
   	return new_obj;
 
 end
 
 function PriometheusEventListener:on_allow_dueto_dont_apply_for(url)
-	self.metric_allowed:inc(1, {url, 'dont_apply_for'})
+	self.metric_allowed:inc(1, {ngx.var.server_name, url, 'dont_apply_for'})
 end
 
 function PriometheusEventListener:on_allow_dueto_only_apply_for(url)
-	self.metric_allowed:inc(1, {url, 'only_apply_for'})
+	self.metric_allowed:inc(1, {ngx.var.server_name, url, 'only_apply_for'})
 end
 
 function PriometheusEventListener:on_deny_dueto_black_list(url)
-	self.metric_denied:inc(1, {url, 'black_list'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'black_list'})
 end
 
 function PriometheusEventListener:on_deny_dueto_unsupported_auth_type(url, auth_header)
-	self.metric_denied:inc(1, {url, 'unsupported_auth_type'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'unsupported_auth_type'})
 end
 
 function PriometheusEventListener:on_deny_dueto_no_anon_rules_found(url)
-	self.metric_denied:inc(1, {url, 'no_anon_rules_found'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'no_anon_rules_found'})
 end
 
 function PriometheusEventListener:on_deny_dueto_no_anon_config(url)
-	self.metric_denied:inc(1, {url, 'no_anon_config'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'no_anon_config'})
 end
 
 function PriometheusEventListener:on_allow_anon(url)
-	self.metric_allowed:inc(1, {url, 'anon'})
+	self.metric_allowed:inc(1, {ngx.var.server_name, url, 'anon'})
 end
 
 function PriometheusEventListener:on_deny_dueto_no_basic_config(url)
-	self.metric_denied:inc(1, {url, 'no_basic_config'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'no_basic_config'})
 end
 
 function PriometheusEventListener:on_deny_dueto_wrong_basic_pass(url, user_id)
-	self.metric_denied:inc(1, {url, 'wrong_basic_pass'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'wrong_basic_pass'})
 end
 
 function PriometheusEventListener:on_allow_basic(url, user_id)
-	self.metric_allowed:inc(1, {url, 'basic'})
+	self.metric_allowed:inc(1, {ngx.var.server_name, url, 'basic'})
 end
 
 function PriometheusEventListener:on_deny_dueto_no_basic_rules_found(url, user_id)
-	self.metric_denied:inc(1, {url, 'no_basic_rules_found'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'no_basic_rules_found'})
 end
 
 function PriometheusEventListener:on_deny_dueto_no_rbac_config(url)
-	self.metric_denied:inc(1, {url, 'no_rbac_config'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'no_rbac_config'})
 end
 
 function PriometheusEventListener:on_deny_no_rbac_rules_found(url, http_method, sub)
-	self.metric_denied:inc(1, {url, 'no_rbac_rules_found'})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'no_rbac_rules_found'})
 end
 
 function PriometheusEventListener:on_allow_rbac(url, http_method, sub)
-	self.metric_allowed:inc(1, {url, 'rbac'})
+	self.metric_allowed:inc(1, {ngx.var.server_name, url, 'rbac'})
 end
 
 function PriometheusEventListener:on_deny_rbac_token(url, host, error_code, error_reason)
-	self.metric_denied:inc(1, {url, 'rbac_token_' .. error_code})
+	self.metric_denied:inc(1, {ngx.var.server_name, url, 'rbac_token_' .. error_code})
 end
 
 return PriometheusEventListener;
