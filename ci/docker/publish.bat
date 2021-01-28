e9d8743182caed28aaa2414ec5b65e0cc2a1591e
Andrey O <ozzy.ext@gmail.com>
2021-01-28 12:49:12 +0300

    Add base docker deploy

---
+++ ci/docker/publish.bat
@@ -0,0 +1,21 @@
+echo off
+
+IF [%1]==[] goto noparam
+
+echo "Build image ..."
+docker build --no-cache --build-arg MYAUTH_LUA_VERSION=1.2.2 -t ozzyext/myauth-lua-host:%1 -t ozzyext/myauth-lua-host:latest .
+
+echo "Publish image '%1' ..."
+docker push ozzyext/myauth-lua-host:%1
+
+echo "Publish image 'latest' ..."
+docker push ozzyext/myauth-lua-host:latest
+
+goto done
+
+:noparam
+echo "Please specify image version"
+goto done
+
+:done
+echo "Done!"
